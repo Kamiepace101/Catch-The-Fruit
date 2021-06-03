04d093776a1266a53a4e38032e0044dfb2f477c3
Kamiepace101 <74590891+Kamiepace101@users.noreply.github.com>
2021-06-03 12:47:53 +0200

    Apple update
    Made the apples fall and the player is able to collect them

--- Assets/Scripts/Apple.cs
+++ Assets/Scripts/Apple.cs
@@ -4,12 +4,37 @@ using UnityEngine;
 
 public class Apple : MonoBehaviour
 {
-    private float apple = 0;
+    private float _speed = 3f;
+    
+    // Strat is called before the first frame update
+    void Start() {
+        
+    }
+    private void Update() 
+    {
+        
+    }
+
     private void OnTriggerEnter2D(Collider2D other) 
     {
         if(other.transform.tag =="Apple"){
             Destroy(other.gameObject);
         }
-        
+        transform.Translate(Vector3.down * _speed * Time.deltaTime);
+
+        if(transform.position.y < -6.67f)
+        {
+            Destroy(other.gameObject);
+        }
+    }
+    
+    private void FallingApples()
+    {
+        ScoreKeeper scoreKeeper = GameObject.FindObjectOfType<ScoreKeeper>();
+        if(scoreKeeper != null)
+        {
+            scoreKeeper.IncrementScore();
+        }
+        Destroy(gameObject);
     }
 }
