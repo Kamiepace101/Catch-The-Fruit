57062f59c5fffe7507fc51a1f2087fbb20a1f6b0
Kamiepace101 <74590891+Kamiepace101@users.noreply.github.com>
2021-06-03 09:46:59 +0200

    Update spawner
    Updated the spawner Made the apples spawn but still need to make them spawn down words.

---
+++ Assets/Scripts/Apple.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Apple : MonoBehaviour
+{
+    private float apple = 0;
+    private void OnTriggerEnter2D(Collider2D other) 
+    {
+        if(other.transform.tag =="Apple"){
+            Destroy(other.gameObject);
+        }
+        
+    }
+}
