f0ef49144bf2b3c5dd3849cbe88c0ac4426712b3
Kamiepace101 <74590891+Kamiepace101@users.noreply.github.com>
2021-06-03 08:46:47 +0200

    Created a spawner
    I have created a spawner.
    Imported the new stripe that will be used and spawn randomly.
    But for now the spawner does not work.

---
+++ Assets/Scripts/TimedSpawn.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimedSpawn : MonoBehaviour
+{
+    public GameObject Spawner;
+    public bool _stopSpawning = false;
+    public float spawnTime;
+    public float spawnDelay;
+    [SerializeField]
+    private Transform[] SpawnPos;
+    int random;
+
+// Start is called before the first frame update
+    void Start() 
+    {
+        Time.timeScale = 1f;
+        InvokeRepeating("SpawnObject", spawnTime, spawnDelay);
+    }
+
+    public void SpawnObject()
+    {
+        int randPosition = Random.Range(0,SpawnPos.Length);
+        Instantiate(Spawner,SpawnPos[randPosition].position,Quaternion.identity);
+    }
+
+    public void StopSpawning()
+    {
+        Debug.Log("IT should stop spawning");
+        CancelInvoke("SpawnObject");
+    }
+}
